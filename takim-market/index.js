8f126ae331ac508fecc57338157b565b4813d058
xogml123 <xogml951@gmail.com>
2021-10-28 21:25:45 +0900

    feat:make prototype web page using only html and css

---
+++ takim-market/index.js
@@ -0,0 +1,18 @@
+var allProducts = [{
+	name : '농구공',
+	price : 100000,
+	seller : '조던',
+	imageUrl: './images/products/basketball1.jpeg'
+},{
+	name : '축구공',
+	price : 50000,
+	seller : '메시',
+	imageUrl: './images/products/soccerball1.jpg'
+},{
+	name : '키보드',
+	price : 10000,
+	seller : '그랩',
+	imageUrl: './images/products/keyboard1.jpg'
+}];
+
+document.querySelector("#product-list").innerHTML;
